982c672ceb76ff74b89add5efad5b2919f900523
matheusfladislau <matheus.ladislaudesenv@gmail.com>
2024-09-17 21:14:01 -0300

    feat: spaceship bounds

--- Assets/Scripts/Spaceship.cs
+++ Assets/Scripts/Spaceship.cs
@@ -2,8 +2,31 @@ using UnityEngine;
 
 public class Spaceship : MonoBehaviour
 {
+    private Bounds _cameraBounds;
+    private SpriteRenderer _renderer;
     public Projectile projectilePrefab;
     public float speed = 5f;
+
+    private void Start()
+    {
+        _renderer = GetComponent<SpriteRenderer>();
+
+        var height = Camera.main.orthographicSize * 2;
+        var width = height * Camera.main.aspect;
+
+        _cameraBounds = new Bounds(Vector3.zero, new Vector3(width, height));
+    }
+
+    private void LateUpdate() 
+    {
+        var spriteWidth  = _renderer.sprite.bounds.extents.x;
+        var spriteHeight = _renderer.sprite.bounds.extents.y;
+
+        var newPosition = transform.position;
+        newPosition.x = Mathf.Clamp(newPosition.x, _cameraBounds.min.x + spriteWidth, _cameraBounds.max.x - spriteWidth);
+        newPosition.y = Mathf.Clamp(newPosition.y, _cameraBounds.min.y + spriteHeight, _cameraBounds.max.y - spriteHeight);
+        transform.position = newPosition;
+    }
 
     void Update()
     {
